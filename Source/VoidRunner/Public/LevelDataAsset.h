9151ba9b0c442f3d865feedc62af624c05ebab99
Sapodilla <jakebaxter8@hotmail.co.uk>
2023-08-06 02:18:03 +0100

    Added LevelDataAsset

---
+++ Source/VoidRunner/Public/LevelDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOIDRUNNER_API ULevelDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
